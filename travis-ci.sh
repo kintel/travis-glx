c50a416c249c6485193b11f0c61ac5830c740e12
Marius Kintel <marius@kintel.net>
2016-08-21 13:27:46 -0400

    purge

--- travis-ci.sh
+++ travis-ci.sh
@@ -7,11 +7,10 @@ ldd /usr/bin/glxinfo
 
 ./virtualfb.sh
 glxinfo -display `cat virtualfb.DISPLAY`
+RET=$?
 ./virtualfb.sh
 
-ls -l /var/log
-
-if [[ $? != 0 ]]; then
+if [[ $RET != 0 ]]; then
   echo "Test failure"
   exit 1
 fi
